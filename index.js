074625bfa81e994bbfd2a952362b7580032fe8cb
alok <saxena>
2018-06-30 01:49:39 +0530

    pakage

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var express = require('express'),
         port = 3000;
 
 
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
